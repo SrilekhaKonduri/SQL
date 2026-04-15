a77e0a8fb2fd4caa74ac3c523e4f2c0f62faab79
Srilekha Konduri <kondurisrilekha888@gmail.com>
2026-04-15 15:51:47 +0530

    push

---
+++ project_sql/top_demanded_skills.sql
@@ -0,0 +1,5 @@
+select *
+from job_postings_fact
+inner join skills_job_dim on job_postings_fact.job_id=skills_job_dim.job_id
+inner join skills_dim on skills_job_dim.skill_id=skills_dim.skill_id
+limit 5
